e4e2439763037ed8731df66ebdd714cc28941205
강은석 <gdeom@mutosmart.com>
2025-08-18 15:01:11 +0900

    feat: "대출" 관련 API 기능 추가"

--- src/main/java/rental/dto/RentalRequestDTO.java
+++ src/main/java/rental/dto/RentalRequestDTO.java
@@ -1,4 +1,18 @@
 package rental.dto;
 
-public class RentalRequestDTO {
+import book.entity.Books;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import user.entity.Users;
+
+public record RentalRequestDTO(
+        Long id,
+        Books book,
+        Users user,
+        LocalDateTime rentalDate,
+        LocalDateTime returnDate,
+        boolean returned
+) {
 }
